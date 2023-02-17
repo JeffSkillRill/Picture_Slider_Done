d349ebd2645481bcef31f455b523efae21737ba0
Acer <abdujafar.rashidov.2006@gmail.com>
2023-02-18 01:39:21 +0500

    Bug_Fixed_Working

---
+++ app/src/main/java/com/example/jeff_skillrill/Secon_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.jeff_skillrill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Secon_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secon)
+    }
+}
